6d77f000d8bb98d6533af43ecc7aa1bd0e7a3284
Taly <trabinerson@paypal.com>
2016-09-21 11:48:43 +0300

    Added basic (still non functional) amount layout

---
+++ app/src/main/java/com/example/trabinerson/cashbox/AmountActivity.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2016 PayPal, Inc.
+ *
+ * All rights reserved.
+ *
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ */
+
+package com.example.trabinerson.cashbox;
+
+/**
+ * Selector for withdrawal amount.
+ *
+ * @author trabinerson
+ */
+public class AmountActivity {
+
+}
